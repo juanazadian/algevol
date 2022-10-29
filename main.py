7240f6e44d7f5b4aa4c5d64017d4a57efe64856a
Juan Martin Azadian <juanazadian@gmail.com>
2022-10-29 11:54:02 -0300

    Copied example

---
+++ main.py
@@ -0,0 +1,40 @@
+#!/bin/env python
+
+from jmetal.algorithm.multiobjective.spea2 import SPEA2
+from jmetal.operator import PolynomialMutation, SBXCrossover
+from jmetal.problem import DTLZ2
+from jmetal.util.solution import (
+    print_function_values_to_file,
+    print_variables_to_file,
+    read_solutions,
+)
+from jmetal.util.termination_criterion import StoppingByEvaluations
+
+from jmetal.lab.visualization.plotting import Plot
+from jmetal.util.solution import get_non_dominated_solutions
+
+if __name__ == "__main__":
+    problem = DTLZ2()
+    problem.reference_front = read_solutions(filename="resources/reference_front/DTLZ2.3D.pf")
+
+    max_evaluations = 20000
+    algorithm = SPEA2(
+        problem=problem,
+        population_size=20,
+        offspring_population_size=20,
+        mutation=PolynomialMutation(probability=1.0 / problem.number_of_variables, distribution_index=20),
+        crossover=SBXCrossover(probability=1.0, distribution_index=20),
+        termination_criterion=StoppingByEvaluations(max_evaluations=max_evaluations),
+    )
+
+    algorithm.run()
+    solutions = algorithm.get_result()
+
+    print(f"Algorithm: {algorithm.get_name()}")
+    print(f"Problem: {problem.get_name()}")
+    print(f"Computing time: {algorithm.total_computing_time}")
+
+    front = get_non_dominated_solutions(solutions)
+
+    plot_front = Plot(plot_title='Pareto front approximation', axis_labels=['x', 'y'])
+    plot_front.plot(front, label='SPEA2-ZDT1')
