01419c755129a49c32c5021f7713bcda2b80e5c9
nico-mautone <nicomautone13@gmail.com>
2022-11-10 19:56:21 -0300

    Start Mutation

--- mutation.py
+++ mutation.py
@@ -13,7 +13,23 @@ class GraphMutation(Mutation[GraphSolution]):
     def execute(self, solution: GraphSolution) -> GraphSolution:
         Check.that(issubclass(type(solution), GraphSolution), "Solution type invalid")
 
-        # modifico "sulution" con la mutacion que dijimos, y la devuelvo.
+        # # random number between 1 and number_of_variables
+        # picked_nbh = random.randint(1, solution.number_of_variables - 1) # No se está considerando la central. Podría considerarse tambien.
+
+        # possible_neighbors = self.grafo_barrios_montevideo[picked_nbh]
+
+        # picked_neighbor = random.sample(possible_neighbors, 1)[0]
+
+        # if picked_neighbor in solution.variables[picked_nbh]:
+        #     solution.variables[picked_nbh].remove(picked_neighbor)
+        #     solution.variables[picked_neighbor].remove(picked_nbh)
+        # else:
+        #     solution.variables[picked_nbh] += [picked_neighbor]
+        #     solution.variables[picked_neighbor] += [picked_nbh]
+
+        # # modifico "sulution" con la mutacion que dijimos, y la devuelvo.
+        # print(solution)
+
         return solution
 
     def get_name(self):
