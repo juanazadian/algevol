41c54084d1480777bc7497a9b7c164ceb738e229
nico-mautone <nicomautone13@gmail.com>
2022-12-10 12:14:15 -0300

    some fixes

--- helpers.py
+++ helpers.py
@@ -1,4 +1,9 @@
 import copy
+from jmetal.util.solution import (
+    read_solutions,
+    print_function_values_to_file,
+    print_variables_to_file,
+)
 
 def dfs(visited, graph, node):
     if node not in visited:
@@ -21,3 +26,8 @@ def negative_correction(solution):
             if index not in [nbh for nbh in solution.variables[neighbor]]:
                 res.variables[index].remove(neighbor)
     return res
+
+
+def get_border_solutions_pareto(pareto_file='evaluation/reference_pareto/FUN.PARETO_DFOM_SPEA2-NEIGHBORHOODS_GRAPH_CENTRAL_40'):
+    pareto_front = read_solutions(pareto_file)
+    print("pareto_front ", pareto_front)
